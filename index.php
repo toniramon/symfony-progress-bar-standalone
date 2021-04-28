d2d8118202d74cfa0eba71525788aa5d13b4582f
Toni Ramon <toniramon@MacBook-Pro-de-Toni.local>
2021-04-28 21:09:25 +0200

    added elapsed, remaining and memory to status bar

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 
 $output = new ConsoleOutput();
@@ -16,23 +17,33 @@ $output->writeln(
 
 $outputStyle = new OutputFormatterStyle('red', '#ff0', ['bold', 'blink']);
 $output->getFormatter()->setStyle('fire', $outputStyle);
-
 $output->writeln('<fire>foo</>');
 
-// ... all the other code from before
-
+// Magic happens here.
 $rows = 27000;
 $progressBar = new ProgressBar($output, $rows);
 $progressBar->setBarCharacter('<fg=magenta>=</>');
+
+$progressBar->setFormat(
+            "%current%/%max% [%bar%] %percent:3s%%
+            \n Working 🛠: %elapsed:-2s% || ETA ⏳: %remaining:-2s% || Mem 💾: %memory:-2s% "
+        );
+
+
+$output->writeln("\n Starting Item Importer.. \n");
+
+
+// Set a moving. 🍺 
 $progressBar->setProgressCharacter("\xF0\x9F\x8D\xBA");
 
 for ($i = 0; $i<$rows; $i++) {
     // Your beautiful code here.
-    usleep(100);
+    usleep(500);
 
     $progressBar->advance();
 }
 
 $progressBar->finish();
-$output->writeln('');
-$table->render();
+
+
+$output->writeln("\n \n All items has been imported! \n");
